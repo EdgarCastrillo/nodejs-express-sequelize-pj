5654eb0edca3e4258d145017ec8ca37743a3bec6
Edgar Castrillo <edgarcastrillo@icloud.com>
2019-09-11 12:53:19 +0200

    new testimonial model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // Importar express
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const routes = require('./routes')
 
 const configs = require('./config')
@@ -34,6 +35,9 @@ app.use((req, res, next) => {
   res.locals.fechaActual = fecha.getFullYear()
   return next()
 })
+
+// Ejecutar body parser
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Cargar rutas
 app.use('/', routes())
--- server/routes/index.js
+++ server/routes/index.js
@@ -32,5 +32,43 @@ module.exports = function() {
       .catch(error => console.log(error))
   })
 
+  router.get('/testimoniales', (req, res) => {
+    res.render('testimoniales', {
+      pagina: 'Testimoniales'
+    })
+  })
+  
+  // Cuando se llena el formularioo
+  router.post('/testimoniales', (req, res) => {
+    console.log(req.body)
+    // Validar que todos los campos esten llenos
+    let {nombre, correo, mensaje} = req.body
+
+    let errores = []
+    if(!nombre) {
+      errores.push({'mensaje' : 'Agrega tu Nombre'})
+    }
+    if(!correo) {
+      errores.push({'mensaje' : 'Agrega tu Correo'})
+    }
+    if(!mensaje) {
+      errores.push({'mensaje' : 'Agrega tu Mensaje'})
+    }
+
+    // revisar por errores
+    if(errores.length > 0) {
+      // Muestra la vista con errores
+      res.render('testimoniales', {
+        errores,
+        nombre,
+        correo,
+        mensaje
+      })
+    } else {
+      // Almacenarlo en la BD
+    }
+
+  })
+
   return router
 }
